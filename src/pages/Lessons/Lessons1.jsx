8ba8cca81b3846ffdb6206743eaeb9204be1bb3f
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-06-27 13:39:45 -0500

    Reto 1

--- src/pages/Lessons/Lessons1.jsx
+++ src/pages/Lessons/Lessons1.jsx
@@ -13,6 +13,15 @@ export const Lessons1 = () => {
         <div className="contenedor__item">5</div>
         <div className="contenedor__item">6</div>
       </div>
+      <h2 className="Title">Reto 1</h2>
+      <div className="reto__contenedor">
+        <div className="reto__contenedor__item">1</div>
+        <div className="reto__contenedor__item">2</div>
+        <div className="reto__contenedor__item">3</div>
+        <div className="reto__contenedor__item">4</div>
+        <div className="reto__contenedor__item">5</div>
+        <div className="reto__contenedor__item">6</div>
+      </div>
     </LayoutLessons>
   );
 };
