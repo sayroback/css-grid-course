cb4afd32bd36685c63944131b1b52aad2b0dd97e
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-07-19 01:46:28 -0500

    finished cover

--- src/pages/PortadaProject/index.jsx
+++ src/pages/PortadaProject/index.jsx
@@ -2,5 +2,20 @@ import React from "react";
 import "./styles/portada.scss";
 
 export const PortadaProject = () => {
-  return <div className="portada"></div>;
+  return (
+    <div className="portada">
+      <div className="item-1"></div>
+      <div className="item-2"></div>
+      <div className="item-3"></div>
+      <div className="item-4"></div>
+      <div className="item-5"></div>
+      <div className="item-6">
+        <p>CSS Grid</p>
+      </div>
+      <div className="item-7"></div>
+      <div className="item-8">
+        <p>Portada</p>
+      </div>
+    </div>
+  );
 };
